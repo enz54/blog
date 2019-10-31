d3694cf757deff6eec7d486b856a7ab10968f5bb
enz54 <ericnzenga@gmail.com>
2019-10-31 08:50:13 +0100

    UPDATE modified pages and component

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,16 @@
 import React from "react"
-import styles from "./404.module.css"
+import styles from "./404.module.scss"
 import Layout from "../components/layout"
+import {Link} from 'gatsby'
 
 export default () => (
   <Layout>
-    <div>
+    <div className={styles.content}>
       <h1 className={styles.header}>Page not found</h1>
-      <p clù={styles.errorMessage}>
+      <p className={styles.errorMessage}>
         The page you are looking for, does not exist.
       </p>
+      <Link to='/'>Home</Link>
     </div>
   </Layout>
 )
